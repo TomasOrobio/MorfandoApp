7271c30bd83fb0aca825b67c8cbd484d8871472a
agustin.maggi <agustin.maggi@civicplus.com>
2022-11-18 20:34:29 -0300

    Update libs

--- App.tsx
+++ App.tsx
@@ -1,33 +1,17 @@
-/* eslint-disable eol-last */
-/* eslint-disable react/jsx-no-undef */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
-/* eslint-disable prettier/prettier */
-/* eslint-disable react-native/no-inline-styles */
-import 'react-native-gesture-handler';
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Text, View } from 'react-native';
-import MenuItem from './src/design/screens/user/MenuItem';
-import HomeScreen from './src/design/screens/user/HomeScreen';
-import Perfil from './src/design/screens/user/Profile';
-import Favoritos from "./src/design/screens/user/Favoritos";
-import LoginPage from "./src/design/screens/user/LoginPage";
-import {TabNavigation} from './src/design/components/TabNavigation';
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+import { AuthProvider } from './src/provider/AuthProvider';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-const App = () => {
-  return (
-
-    <>
-    <NavigationContainer>
-      <TabNavigation/>
-    </NavigationContainer>
-
-    </>
-
-  );
-};
-
-export default App;
+export default function App() {
+	return (
+		<SafeAreaProvider>
+			<AuthProvider>
+				<BottomSheetModalProvider>
+					<AppNavigator />
+				</BottomSheetModalProvider>
+			</AuthProvider>
+		</SafeAreaProvider>
+	);
+}
--- ios/MorfandoAppTests/MorfandoAppTests.m
+++ ios/MorfandoAppTests/MorfandoAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MorfandoAppTests : XCTestCase
+@interface morfandoappTests : XCTestCase
 
 @end
 
-@implementation MorfandoAppTests
+@implementation morfandoappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
